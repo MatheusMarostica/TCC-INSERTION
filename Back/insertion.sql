c04ac3620d167723c626b3bacd69ae2b05c143bf
Matheus Maróstica <124899689+MatheusMarostica@users.noreply.github.com>
2024-10-07 12:23:47 -0300

    inserindo códigos 7/1

--- Back/insertion.sql
+++ Back/insertion.sql
@@ -1,14 +1,6 @@
 create database db_insertion;
 use db_insertion;
 drop database db_insertion;
-create table tasks(
-	id INT auto_increment primary key,
-    email VARCHAR(255) unique not null,
-    senha VARCHAR(255) not null
-);
-
-select*from tasks;
-drop table tasks;
 
 create table user_table(
 	id INT auto_increment primary key,
