76b3bfdf6fdc2ff19d495529c91815f685aab9bc
Matheus Maróstica <124899689+MatheusMarostica@users.noreply.github.com>
2024-10-07 12:25:48 -0300

    atualizando routers

--- Back/src/routes/feedrouter.js
+++ Back/src/routes/feedrouter.js
@@ -1,14 +1,8 @@
-// Importa o framework Express.js
 const express = require("express");
-
-// Cria uma instância do router do Express.js
 const router = express.Router();
+const { storefeed, getFeed } = require("../controller/feedController");
 
-// Importa a função storefeed do controller feedController
-const { storefeed } = require("../controller/feedController");
-
-// Define uma rota POST para /store/feed que chama a função storefeed
 router.post("/store/feed", storefeed);
+router.get("/get/feed", getFeed);
 
-// Exporta o router para que possa ser utilizado em outros módulos
 module.exports = router;
--- Back/src/routes/taskrouter.js
+++ Back/src/routes/taskrouter.js
@@ -1,14 +1,7 @@
-// Importa o módulo Router do Express.js e desestrutura-o para obter a classe Router
 const { Router } = require("express");
-
-// Cria uma instância da classe Router
 const router = Router();
-// Importa a função storeTask do controller taskController
 const { storeTask } = require("../controller/taskController");
 
-// Define uma rota POST para /post/enviar que chama a função storeTask
-// Essa rota é responsável por lidar com requisições de envio de tarefas
 router.post("/post/enviar", storeTask);
 
-// Exporta o router para que possa ser utilizado em outros módulos
 module.exports = router;
